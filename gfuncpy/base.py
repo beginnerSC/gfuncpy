5b804a924b4377ee1c6be596bc897dd4672313c6
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2023-05-26 02:16:53 +0000

    autosave

--- gfuncpy/base.py
+++ gfuncpy/base.py
@@ -10,14 +10,15 @@ if __name__ == '__main__':
     # z = Identity(0, 2*pi, 100)
     # sin(x) + cos(z)
 
-    # can't find a way for this to work
-    # h = 2 + cos(x)
+    # can't find a way for this to work: h = 2 + cos(x)
+    # but there is a way: 2*np.arange(5) works
 
     x = Identity.uniform_grid(0, 2*pi, 100)
     f = sin(x) + cos(x)
-    g = cos(x) + 2
+    g = sin(x) + sin(x) + cos(x)
+    h = cos(x) + 2
     
-    print(f(pi/2), g(pi/2))
+    print(f(pi/2), g(pi/2), h(pi/2))
     f.plot()
     g.plot()
 
--- gfuncpy/gfuncs.py
+++ gfuncpy/gfuncs.py
@@ -4,12 +4,55 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 
 class GridFunction:
+    '''
+    Assuming x is sorted and y is in the corresponding order
+    '''
     def __init__(self, x=None, y=None):
         if x is not None and y is not None and not (hasattr(x, '__len__') and hasattr(y, '__len__') and len(x)==len(y)):
             raise ValueError('x and y must be array-like object with the same length')
         self.x = x
         self.y = y
     
+    @classmethod
+    def from_dataframe(cls, df, y, x=None):
+        '''
+        must specify y (a column name)
+        if x is not specified will use index of the DataFrame
+        '''
+        f = cls()
+        f.y = df[y].values
+        
+        if x is None:
+            f.x = df.index.values
+        else:
+            f.x = df[x].values
+        return f
+        
+    @classmethod
+    def from_series(cls, ss):
+        f = cls()
+        f.x = ss.index.values
+        f.y = ss.values
+        return f
+        
+    def root(self):
+        '''
+        Corner case handeling: What if no root? 
+
+        can't use np.searchsorted: x is sorted but not y
+        '''
+        pass
+    
+        # if self.y[0]==0:
+        #     return self.x[0]
+        # elif self.y[0] < 0:
+        #     idx = np.searchsorted(self.y, 0)
+        # elif self.y[0] > 0:
+        #     idx = np.searchsorted(-self.y, 0)
+
+        # x0, x1, y0, y1 = self.x[idx-1], self.x[idx], self.y[idx-1], self.y[idx]
+        # return x0 + (x1-x0)*abs(y0/(y1-y0))
+        
     def __call__(self, x):
         '''
         x can be a list
@@ -18,12 +61,10 @@ class GridFunction:
         return intp(x)
     
     def __add__(self, other):
-
-        f = copy.deepcopy(self)
-        
+        f = GridFunction(x=self.x, y=np.copy(self.y))
         if isinstance(other, GridFunction):
             if id(self.x) != id(other.x):
-                raise ValueError("The two functions for the operation must share the same x grid instance. ")
+                raise ValueError("The two functions for this operation must share the same x grid instance. ")
             f.y += other.y
         else:
             # other is a number
@@ -31,6 +72,18 @@ class GridFunction:
 
         return f
 
+    def __sub__(self, other):
+        f = GridFunction(x=self.x, y=np.copy(self.y))
+        if isinstance(other, GridFunction):
+            if id(self.x) != id(other.x):
+                raise ValueError("The two functions for this operation must share the same x grid instance. ")
+            f.y -= other.y
+        else:
+            # other is a number
+            f.y -= other
+
+        return f
+    
     def plot(self, style='-'):
         plt.plot(self.x, self.y, style)
         
@@ -43,3 +96,4 @@ class Identity(GridFunction):
         f.x = np.linspace(a, b, n+1)
         f.y = np.linspace(a, b, n+1)
         return f
+
